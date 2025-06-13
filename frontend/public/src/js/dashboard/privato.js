e4591f5d20e4e928da51b7f506e4399737de454a
Fabio Compagnoni <fabiocompagnoni5@protonmail.com>
2025-06-14 00:19:34 +0200

    pagina ordine con elenco prodotti e principali informazioni

--- frontend/public/src/js/dashboard/privato.js
+++ frontend/public/src/js/dashboard/privato.js
@@ -133,14 +133,150 @@ const makeDivOrder=(order)=>{
     return cont;
 }
 
+const makeDivProdotto=(prodotto)=>{
+    let cont=document.createElement("div");
+    cont.classList.add("row", "product", "shadow");
+
+    let img=document.createElement("img");
+    img.dataset.src=prodotto.thumbnail;
+    img.classList.add("lazyImages", "productImg");
+    loadImage(img);
+    
+    let name=document.createElement("div");
+    name.classList.add("productName");
+    name.innerHTML=prodotto.name;
+    let quantity=document.createElement("div");
+    quantity.classList.add("quantity");
+    quantity.innerHTML=`${prodotto.quantity} pezzi`;
+
+    let priceSingle=document.createElement("div");
+    priceSingle.classList.add("priceSingle");
+    priceSingle.innerHTML=`Prezzo unitario ${parseFloat(prodotto.price).toLocaleString("it-IT", { style: 'currency', currency: 'EUR'})}`;
+
+    let priceTot=document.createElement("div");
+    priceTot.classList.add("priceTot");
+    priceTot.innerHTML=`Totale ${parseFloat(prodotto.price * prodotto.quantity).toLocaleString("it-IT", { style: 'currency', currency: 'EUR'})}`;
+
+    let artisan=document.createElement("a");
+    artisan.href=prodotto.artisan.link;
+    artisan.classList.add("artisan");
+    let imgArtisan=document.createElement("img");
+    imgArtisan.dataset.src=prodotto.artisan.photoProfile;
+    imgArtisan.classList.add("lazyImages", "artisanImg");
+    loadImage(imgArtisan);
+    artisan.appendChild(imgArtisan);
+    let nameArtisan=document.createElement("div");
+    nameArtisan.classList.add("nameArtisan");
+    nameArtisan.innerHTML=`${prodotto.artisan.name} ${prodotto.artisan.surname}`;
+    artisan.appendChild(nameArtisan);
+
+    let col1=document.createElement("div");
+    col1.classList.add("col-md-4");
+    col1.appendChild(img);
+    
+    let col2=document.createElement("div");
+    col2.classList.add("col-md-6", "my-2","my-lg-0");
+    col2.appendChild(name);
+    col2.appendChild(quantity);
+    col2.appendChild(priceSingle);
+    col2.appendChild(priceTot);
+    col2.appendChild(artisan);
+
+    cont.appendChild(col1);
+    cont.appendChild(col2);
+    return cont;
+}
+
+let mockedProducts=[
+    {
+        id: 1,
+        name: "Maglietta",
+        quantity: 2,
+        price: 25.00,
+        thumbnail: "https://via.placeholder.com/150",
+        artisan: {
+            name: "Mario",
+            surname: "Rossi",
+            photoProfile: "https://via.placeholder.com/50",
+            link: "/artigiani/mario-rossi"
+        }
+    },
+    {
+        id: 2,
+        name: "Pantalone",
+        quantity: 1,
+        price: 50.00,
+        thumbnail: "https://via.placeholder.com/150",
+        artisan: {
+            name: "Luigi",
+            surname: "Verdi",
+            photoProfile: "https://via.placeholder.com/50",
+            link: "/artigiani/luigi-verdi"
+        }
+    },
+    {
+        id: 3,
+        name: "Scarpe",
+        quantity: 1,
+        price: 100.00,
+        thumbnail: "https://via.placeholder.com/150",
+        artisan: {
+            name: "Giuseppe",
+            surname: "Bianchi",
+            photoProfile: "https://via.placeholder.com/50",
+            link: "/artigiani/giuseppe-bianchi"
+        }
+    }
+]
+
+const mokedOrder={
+    id:1,
+    timestamp:`${new Date().getDate()}/${new Date().getMonth()+1}/${new Date().getFullYear()} ${new Date().getHours()}:${new Date().getMinutes()}`,
+}
+const loadOrderProducts=async()=>{
+    try{
+        //sostitutire con chiamata api backend
+        let listProduct=document.getElementById("listProducts");
+        listProduct.innerHTML="";
+        let totale=0;
+        let contProdotti=0;
+        mockedProducts.forEach((prodotto)=>{
+            listProduct.appendChild(makeDivProdotto(prodotto));
+            totale+=prodotto.price*prodotto.quantity;
+            contProdotti+=prodotto.quantity;
+        });
+
+        document.getElementById("orderTotale").innerHTML=parseFloat(totale).toLocaleString("it-IT", { style: 'currency', currency: 'EUR' });
+        document.getElementById("orderTotale").classList.remove("placeholder-glow");
+        document.getElementById("numProducts").innerHTML=`${contProdotti} prodotti`;
+        document.getElementById("numProducts").classList.remove("placeholder-glow");
+    }catch(err){
+        console.error(err);
+    }
+}
+
+const loadOrder=(idOrder)=>{
+    //TODO: sostituire con API
+    try{
+        const order=mokedOrder;
+        loadOrderProducts();
+        document.getElementById("orderTimestamp").innerHTML=order.timestamp;
+        document.getElementById("orderTimestamp").classList.remove("placeholder-glow")
+        
+    }catch(err){
+        console.error(err);
+    }
+}
 const loadOrders=async(preview=false)=>{
     let cont=document.createElement("div");
     cont.classList.add("listOrders");
     let urlRequest=`https://localhost:4000/orders`;
     let response=mockOrders;
-    if(preview)
+    console.log(response);
+    if(preview){
         urlRequest+="/preview";
         response.length=3;
+    }
     try{
         //const request=await ajax(urlRequest);
         response.forEach((order)=>{
@@ -161,10 +297,14 @@ const handlePageHome=async()=>{
 }
 
 const handlePageOrders=async()=>{
-    let divOrders=loadOrders(false);
+    let divOrders=await loadOrders(false);
+    document.getElementById("previewOrdersAll").innerHTML=divOrders.innerHTML;
+
 }
 const handlePageOrder=(idOrder)=>{
-
+    loadOrder(idOrder);
+    //TODO: gestire segnalazione
+    //TODO: gestire reso e spedizione
 }
 const handlePageProfile=()=>{
 
