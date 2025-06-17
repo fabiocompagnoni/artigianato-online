df04cefa08af0c1d455b90d80a5ac07808929f53
Fabio Compagnoni <fabiocompagnoni5@protonmail.com>
2025-06-17 19:09:45 +0200

    integrazione login

--- frontend/public/src/js/login.js
+++ frontend/public/src/js/login.js
@@ -3,6 +3,11 @@ const makeLogin=async()=>{
     let password=document.getElementById("password").value;
     document.getElementById("inputPart").style.display="none";
     document.getElementById("loadingPart").style.display="flex";
+
+    let errorPart=document.getElementById("errorPart");
+    errorPart.innerHTML="";
+    errorPart.style.display="none";
+    
     try{
         const request=await fetch("https://localhost:3000/users/login",{
             method:"POST",
@@ -15,6 +20,28 @@ const makeLogin=async()=>{
                 password:password
             })
         });
+
+        let response=await request.json();
+        if(request.status!=200){
+            errorPart.innerHTML=response.error;
+            errorPart.style.display="block";
+            errorPart.classList.add("text-danger");
+            document.getElementById("loadingPart").style.display="none";
+            document.getElementById("inputPart").style.display="block";
+            return;
+        }else{
+            
+            let urlDashboard="";
+            if(response.role=="admin"){
+                urlDashboard="/admin/area-riservata";
+            }else if(response.role=="customer"){
+                urlDashboard="/clienti/area-riservata";
+            }else if(response.role=="artisan"){
+                urlDashboard="/artigiani/area-riservata";
+            }
+            window.location.href=urlDashboard;
+        }
+
     }catch(err){
         console.error(err);
     }
