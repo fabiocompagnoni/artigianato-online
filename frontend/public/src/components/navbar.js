f5c3e20fa2a79d3bd475be8111af8c18af9d0d62
Fabio Compagnoni <fabiocompagnoni5@protonmail.com>
2025-06-19 22:16:03 +0200

    fix link dashboard page navbar

--- frontend/public/src/components/navbar.js
+++ frontend/public/src/components/navbar.js
@@ -126,27 +126,33 @@ class NavBar extends HTMLElement {
             let btnMobile=document.getElementById("btnUserProfile");
 
             let isLoggedin=false;
-            let req=await fetch("https://localhost:3000/users/isLoggedIn");
+            let req=await fetch("https://localhost:3000/users/isLoggedIn",{
+                method:"GET",
+                credentials: 'include'
+            });
             let respJson=await req.json();
             if(req.status!=200){
                 console.error("Error fetching login status:", respJson.error);
                 return;
             }
-            if(respJson.isLoggedin!=null)
-                isLoggedin=respJson.isLoggedin;
+            if(respJson.loggedIn!=null)
+                isLoggedin=respJson.loggedIn;
             if(isLoggedin){
-                let reqLinkDashboard=await fetch("https://localhost:3000/users/dashboardPage");
+                let reqLinkDashboard=await fetch("https://localhost:3000/users/dashboardPage",{
+                    method:"GET",
+                    credentials: 'include'
+                });
                 let linkDashboard=await reqLinkDashboard.json();
                 if(reqLinkDashboard.status!=200){
                     console.error("Error fetching dashboard link:", linkDashboard.error);
                     return;
                 }
-                if(reqLinkDashboard.dashboardLink!=null){
+                if(linkDashboard.dashboardLink!=null){
                     btnDesktop.onclick=()=>{
-                        window.location.href=reqLinkDashboard.dashboardLink;
+                        window.location.href=linkDashboard.dashboardLink;
                     }
                     btnMobile.onclick=()=>{
-                        window.location.href=reqLinkDashboard.dashboardLink;
+                        window.location.href=linkDashboard.dashboardLink;
                     }
                 }
             }else{
