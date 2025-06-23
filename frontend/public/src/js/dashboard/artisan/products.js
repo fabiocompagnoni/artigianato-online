90d7ac494a17fcace06859808b32cfb4f69d7a63
Fabio Compagnoni <fabiocompagnoni5@protonmail.com>
2025-06-23 17:50:23 +0200

    minor fix products artisan

--- frontend/public/src/js/dashboard/artisan/products.js
+++ frontend/public/src/js/dashboard/artisan/products.js
@@ -27,6 +27,7 @@ export const loadProducts=async(page)=>{
         if(requestProducts.error!=null){
             throw new Error(requestProducts.error);
         }
+        tbody.innerHTML=``;
         requestProducts.products.forEach(product=>{
             let tr=document.createElement("tr");
             let categoriesString = product.categories.map(cat => cat.name).join(", ");
@@ -54,6 +55,9 @@ export const loadProducts=async(page)=>{
             tbody.appendChild(tr);
 
         });
+        if(requestProducts.products.length==0){
+            tbody.innerHTML=`<tr><td colspan='7'>Nessun prodotto ancora caricato</td></tr>`;
+        }
         makePagination(page, requestProducts.pages);
 
     }catch(err){
